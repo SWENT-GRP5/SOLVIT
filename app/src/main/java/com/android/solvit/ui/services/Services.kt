f531591cfa3777e12157abfe32a46ac6c459bb18
Manu Cristini <manu.cristini@epfl.ch>
2024-10-07 11:21:21 +0200

    feat(ui): add search bar in services screen and update ui

--- app/src/main/java/com/android/solvit/ui/services/Services.kt
+++ app/src/main/java/com/android/solvit/ui/services/Services.kt
@@ -1,30 +1,64 @@
 package com.android.solvit.ui.services
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.solvit.R
 import com.android.solvit.model.Services
+import com.android.solvit.model.services.SearchServicesViewModel
+import com.android.solvit.ui.theme.Typography
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ServicesScreen() {
+
+    val searchViewModel = SearchServicesViewModel()
+    val searchText by searchViewModel.searchText.collectAsState()
+    val searchResults by searchViewModel.servicesList.collectAsState()
+
     Scaffold(
         modifier = Modifier.padding(16.dp),
         floatingActionButton = {
@@ -34,14 +68,31 @@ fun ServicesScreen() {
             }
         }
     ) { padding ->
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.padding(padding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        Column(
+            modifier = Modifier.fillMaxSize().padding(padding),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            items(SERVICES_LIST.size) { index ->
-                ServiceItem(SERVICES_LIST[index], onClick = { /*TODO*/ })
+            SearchBar(
+                query = searchText,
+                onQueryChange = searchViewModel::onSearchTextChange,
+                onSearch = searchViewModel::onSearchTextChange,
+                active = false,
+                onActiveChange = { searchViewModel.onToggleSearch() },
+                placeholder = { Text("Search Services") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+                colors = SearchBarDefaults.colors(
+                    containerColor = Color.White
+                )
+            ) {}
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.padding(padding),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(searchResults.size) { index ->
+                    ServiceItem(searchResults[index], onClick = { /*TODO*/ })
+                }
             }
         }
     }
@@ -49,18 +100,24 @@ fun ServicesScreen() {
 
 @Composable
 fun ServiceItem(service: ServicesListItem, onClick: () -> Unit) {
-    Card(
+    OutlinedCard(
         modifier = Modifier
             .aspectRatio(1f)
             .clickable { onClick() }
     ) {
-        Text(
-            text = service.service.toString()
-        )
-        Image(
-            painter = painterResource(id = service.image),
-            contentDescription = service.service.toString(),
-            modifier = Modifier.fillMaxSize()
-        )
+        Box {
+            Image(
+                painter = painterResource(id = service.image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize().alpha(0.3f)
+            )
+            Text(
+                text = service.service.toString().replace("_", " "),
+                modifier = Modifier.align(Alignment.Center),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
