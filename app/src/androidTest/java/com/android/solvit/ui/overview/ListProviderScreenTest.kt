2406982f6caeaf703c61215b3f21e29264fa1e45
jamaly <el.jamaly@epfl.ch>
2024-10-09 14:46:46 +0200

    test(ui): add tests for provider filtering functionality

--- app/src/androidTest/java/com/android/solvit/ui/overview/ListProviderScreenTest.kt
+++ app/src/androidTest/java/com/android/solvit/ui/overview/ListProviderScreenTest.kt
@@ -1,21 +1,26 @@
 package com.android.solvit.ui.overview
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.android.solvit.model.map.Location
 import com.android.solvit.model.provider.Language
 import com.android.solvit.model.provider.ListProviderViewModel
 import com.android.solvit.model.provider.Provider
 import com.android.solvit.model.provider.ProviderRepository
 import com.android.solvit.model.provider.Services
-import com.android.solvit.model.map.Location
 import com.google.firebase.Timestamp
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
 
 class ListProviderScreenTest {
   private lateinit var providerRepository: ProviderRepository
@@ -69,5 +74,50 @@ class ListProviderScreenTest {
     composeTestRule.onNodeWithTag("filterOption").assertIsDisplayed()
     composeTestRule.onNodeWithTag("filterOption").performClick()
     composeTestRule.onNodeWithTag("filterSheet").assertIsDisplayed()
+  }
+
+  @Test
+  fun filterAction() {
+    composeTestRule.setContent { selectProviderScreen(listProviderViewModel) }
+    composeTestRule.onNodeWithTag("filterOption").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("filterOption").performClick()
+    composeTestRule.onNodeWithTag("filterSheet").assertIsDisplayed()
+    composeTestRule.waitUntil(
+        timeoutMillis = 10000L,
+        condition = {
+          composeTestRule.onAllNodesWithTag("filterAct").fetchSemanticsNodes().isNotEmpty() &&
+              composeTestRule.onAllNodesWithTag("filterRating").fetchSemanticsNodes().isNotEmpty()
+        })
+    composeTestRule.onAllNodesWithTag("filterAct")[0].assertIsDisplayed()
+    composeTestRule.onAllNodesWithTag("filterAct")[0].performClick()
+    /*var selectedFields = mutableListOf<String>()
+    var iconsPressed = mutableListOf(false, false)
+    var iconsColor = mutableListOf(Color.White, Color.White)
+    val idx = 0
+    val elem = "ENGLISH"
+    val filterAction: (Provider) -> Boolean = { provider -> provider.languages.contains(Language.ENGLISH) }
+    val defaultFilterAction: (Provider) -> Boolean = { true }
+
+    filterStringFields(
+        selectedFields,
+        iconsPressed,
+        iconsColor,
+        { a -> selectedFields = a.toMutableList() },
+        { b -> iconsPressed = b.toMutableList() },
+        { c -> iconsColor = c.toMutableList() },
+        idx,
+        elem,
+        listProviderViewModel,
+        filterAction,
+        defaultFilterAction
+    )
+
+    assert(selectedFields.contains("ENGLISH"))
+    assertTrue(iconsPressed[0])*/
+    verify(providerRepository).filterProviders(any())
+    composeTestRule.onNodeWithTag("minPrice").isDisplayed()
+    composeTestRule.onNodeWithTag("maxPrice").isDisplayed()
+    composeTestRule.onNodeWithTag("minPrice").performTextInput("20")
+    // verify(providerRepository).filterProviders(any())
   }
 }
