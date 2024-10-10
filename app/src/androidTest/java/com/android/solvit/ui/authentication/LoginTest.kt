ab6f93275748adb6fc8860d0d44131757c23c4c7
jamaly <el.jamaly@epfl.ch>
2024-10-11 00:00:20 +0200

    test : add tests to verify login implementation

---
+++ app/src/androidTest/java/com/android/solvit/ui/authentication/LoginTest.kt
@@ -0,0 +1,62 @@
+package com.github.se.bootcamp.ui.authentication
+
+// ***************************************************************************** //
+// ***                                                                       *** //
+// *** THIS FILE WILL BE OVERWRITTEN DURING GRADING. IT SHOULD BE LOCATED IN *** //
+// *** `app/src/androidTest/java/com/github/se/bootcamp/authentication/`.    *** //
+// *** DO **NOT** IMPLEMENT YOUR OWN TESTS IN THIS FILE                      *** //
+// ***                                                                       *** //
+// ***************************************************************************** //
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.solvit.MainActivity
+import com.android.solvit.model.provider.ListProviderViewModel
+import com.android.solvit.model.provider.Provider
+import com.android.solvit.model.provider.ProviderRepository
+import com.android.solvit.ui.navigation.NavigationActions
+import com.android.solvit.ui.overview.SelectProviderScreen
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+@RunWith(AndroidJUnit4::class)
+class LoginTest : TestCase() {
+    @get:Rule val composeTestRule = createComposeRule()
+    private lateinit var navigationActions: NavigationActions
+
+    // The IntentsTestRule simply calls Intents.init() before the @Test block
+    // and Intents.release() after the @Test block is completed. IntentsTestRule
+    // is deprecated, but it was MUCH faster than using IntentsRule in our tests
+
+
+    @Before
+    fun setUp() {
+
+        navigationActions = mock(NavigationActions::class.java)
+
+    }
+    @Test
+    fun titleAndButtonAreCorrectlyDisplayed() {
+        composeTestRule.setContent { SignInScreen(navigationActions) }
+        composeTestRule.onNodeWithTag("loginTitle").assertIsDisplayed()
+
+
+        composeTestRule.onNodeWithTag("loginButton").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
+    }
+
+}
