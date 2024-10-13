219dc2d8e11116a675aed2346d9e168981965fc1
Yshai <yshai.dinee-baumgarten@epfl.ch>
2024-10-13 21:04:50 +0200

    feat: implement ProviderMapScreen
    - Implemented the ProviderMapScreen.
    - Refactor the code for MapScreens to ensure reusability of components
    - Modified the icon of the marker used for the user's location.

--- app/src/main/java/com/android/solvit/provider/ui/map/ProviderMap.kt
+++ app/src/main/java/com/android/solvit/provider/ui/map/ProviderMap.kt
@@ -1,4 +1,4 @@
-package com.android.solvit.seeker.ui.map
+package com.android.solvit.provider.ui.map
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -8,8 +8,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.android.solvit.seeker.model.provider.ListProviderViewModel
 import com.android.solvit.seeker.ui.navigation.SeekerBottomNavigationMenu
+import com.android.solvit.shared.model.request.ServiceRequestViewModel
 import com.android.solvit.shared.ui.map.MapScreen
 import com.android.solvit.shared.ui.map.RequestLocationPermission
 import com.android.solvit.shared.ui.navigation.LIST_TOP_LEVEL_DESTINATION
@@ -18,19 +18,19 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 
 @Composable
-fun SeekerMapScreen(
-    providerViewModel: ListProviderViewModel = viewModel(factory = ListProviderViewModel.Factory),
+fun ProviderMapScreen(
+    serviceRequestViewModel: ServiceRequestViewModel =
+        viewModel(factory = ServiceRequestViewModel.Factory),
     navigationActions: NavigationActions
 ) {
   val context = LocalContext.current
   val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
   var userLocation by remember { mutableStateOf<LatLng?>(null) }
-  val providers by providerViewModel.providersList.collectAsState()
+  val requests by serviceRequestViewModel.requests.collectAsState()
 
   RequestLocationPermission(context, fusedLocationClient) { location -> userLocation = location }
 
-  val markers = providers.map { LatLng(it.location.latitude, it.location.longitude) }
-
+  val markers = requests.map { LatLng(it.location?.latitude ?: 0.0, it.location?.longitude ?: 0.0) }
   MapScreen(
       userLocation = userLocation,
       markers = markers,
---
+++ app/src/main/java/com/android/solvit/shared/ui/map/MapScreen.kt
@@ -0,0 +1,139 @@
+package com.android.solvit.shared.ui.map
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Location
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import com.android.solvit.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun MapScreen(userLocation: LatLng?, markers: List<LatLng>, bottomBar: @Composable () -> Unit) {
+  Scaffold(
+      content = { paddingValues ->
+        Column(
+            modifier =
+                Modifier.fillMaxSize()
+                    .padding(paddingValues)
+                    .background(color = Color.White, shape = RoundedCornerShape(18.dp))
+                    .testTag("mapScreen"),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+          MapContent(userLocation = userLocation, markers = markers)
+        }
+      },
+      bottomBar = bottomBar,
+  )
+}
+
+@Composable
+fun MapContent(userLocation: LatLng?, markers: List<LatLng>, modifier: Modifier = Modifier) {
+  val cameraPositionState = rememberCameraPositionState()
+
+  // Update camera position when userLocation changes
+  LaunchedEffect(userLocation) {
+    userLocation?.let { cameraPositionState.position = CameraPosition.fromLatLngZoom(it, 10f) }
+  }
+
+  GoogleMap(
+      modifier = modifier.fillMaxSize().testTag("googleMap"),
+      cameraPositionState = cameraPositionState) {
+        markers.forEach { position ->
+          Marker(
+              state = MarkerState(position = position), title = "Marker", snippet = "Description")
+        }
+
+        // Display a marker at the user's location if it's available
+        userLocation?.let {
+          val customIcon =
+              bitmapDescriptorFromVector(LocalContext.current, R.drawable.user_location_marker)
+          Marker(
+              state = MarkerState(position = it),
+              tag = "userLocation",
+              icon = customIcon,
+              draggable = false,
+          )}
+      }
+}
+
+@Composable
+fun RequestLocationPermission(
+    context: Context,
+    fusedLocationClient: FusedLocationProviderClient,
+    onLocationReceived: (LatLng?) -> Unit
+) {
+  val locationPermissionLauncher =
+      rememberLauncherForActivityResult(
+          contract = ActivityResultContracts.RequestPermission(),
+          onResult = { isGranted ->
+            if (isGranted) {
+              getCurrentLocation(fusedLocationClient) { location ->
+                onLocationReceived(LatLng(location.latitude, location.longitude))
+              }
+            }
+          })
+
+  LaunchedEffect(Unit) {
+    if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+        PackageManager.PERMISSION_GRANTED) {
+      getCurrentLocation(fusedLocationClient) { location ->
+        onLocationReceived(LatLng(location.latitude, location.longitude))
+      }
+    } else {
+      locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+  }
+}
+
+@SuppressLint("MissingPermission")
+fun getCurrentLocation(
+    fusedLocationClient: FusedLocationProviderClient,
+    onLocationReceived: (Location) -> Unit
+) {
+  fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+    location?.let { onLocationReceived(it) }
+  }
+}
+
+fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+  val vectorDrawable = AppCompatResources.getDrawable(context, vectorResId)
+  vectorDrawable?.let {
+    it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+    val bitmap = Bitmap.createBitmap(it.intrinsicWidth, it.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    it.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bitmap)
+  }
+  return null
+}
