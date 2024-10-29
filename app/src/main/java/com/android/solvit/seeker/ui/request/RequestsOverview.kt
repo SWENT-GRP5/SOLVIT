1bc99f282544415f2947ddc7460423ba364b90ce
Manu Cristini <manu.cristini@epfl.ch>
2024-10-29 22:52:56 +0100

    fix: change requests overview UI to match figma screen

--- app/src/main/java/com/android/solvit/seeker/ui/request/RequestsOverview.kt
+++ app/src/main/java/com/android/solvit/seeker/ui/request/RequestsOverview.kt
@@ -1,21 +1,25 @@
 package com.android.solvit.seeker.ui.request
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,15 +27,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.android.solvit.R
 import com.android.solvit.seeker.ui.navigation.SeekerBottomNavigationMenu
 import com.android.solvit.shared.model.request.ServiceRequest
 import com.android.solvit.shared.model.request.ServiceRequestStatus
@@ -42,156 +48,164 @@ import com.android.solvit.shared.ui.navigation.Route
 import java.text.SimpleDateFormat
 import java.util.Locale
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun RequestsOverviewScreen(
     navigationActions: NavigationActions,
     requestViewModel: ServiceRequestViewModel = viewModel(factory = ServiceRequestViewModel.Factory)
 ) {
   Scaffold(
-      modifier = Modifier.padding(16.dp).testTag("overviewScreen"),
-      content = { paddingValues ->
-        val requests = requestViewModel.requests.collectAsState()
-        if (requests.value.isEmpty()) {
-          Column(
-              modifier =
-                  Modifier.fillMaxSize()
-                      .padding(paddingValues)
-                      .background(color = Color.White, shape = RoundedCornerShape(18.dp))
-                      .testTag("noServiceRequestsScreen"),
-              verticalArrangement = Arrangement.Center,
-              horizontalAlignment = Alignment.CenterHorizontally,
-          ) {
-            Text(
-                text = "You have no active service request. Create one.",
-                style =
-                    TextStyle(
-                        fontSize = 16.sp,
-                        lineHeight = 24.sp,
-                        fontFamily = FontFamily.Default,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF4E616D),
-                        letterSpacing = 0.5.sp,
-                    ))
-          }
-        } else {
-          LazyColumn(
-              modifier =
-                  Modifier.fillMaxSize()
-                      .padding(paddingValues)
-                      .background(color = Color.White, shape = RoundedCornerShape(18.dp)),
-          ) {
-            // Loop through each request and displays it in the overview layout
-            items(requests.value) { request ->
-              RequestItemRow(
-                  request = request,
-                  onClick = {
-                    requestViewModel.selectRequest(request)
-                    navigationActions.navigateTo(Route.EDIT_REQUEST)
-                  })
-            }
-          }
-        }
-      },
+      modifier = Modifier.testTag("overviewScreen"),
       bottomBar = {
         SeekerBottomNavigationMenu(
             onTabSelect = { navigationActions.navigateTo(it.route) },
             tabList = LIST_TOP_LEVEL_DESTINATION_CUSTOMMER,
             selectedItem = navigationActions.currentRoute())
-      })
+      }) {
+        val requests = requestViewModel.requests.collectAsState()
+
+        Column {
+          TopOrdersSection()
+          CategoriesFiltersSection()
+          if (requests.value.isEmpty()) {
+            NoRequestsText()
+          } else {
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                  items(requests.value) { request ->
+                    RequestItemRow(
+                        request = request,
+                        onClick = {
+                          requestViewModel.selectRequest(request)
+                          navigationActions.navigateTo(Route.EDIT_REQUEST)
+                        })
+                  }
+                }
+          }
+        }
+      }
+}
+
+@Composable
+fun TopOrdersSection() {
+  Row(
+      modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 32.dp),
+      horizontalArrangement = Arrangement.SpaceBetween,
+      verticalAlignment = Alignment.CenterVertically) {
+        Row {
+          Icon(
+              imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+              contentDescription = null,
+          )
+          Spacer(modifier = Modifier.size(22.dp))
+          Text(
+              text = "Orders",
+              fontSize = 20.sp,
+              fontWeight = FontWeight.Bold,
+          )
+        }
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = null,
+        )
+      }
+}
+
+@Composable
+fun NoRequestsText() {
+  Column(
+      modifier = Modifier.fillMaxSize().testTag("noServiceRequestsScreen"),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    Text(
+        text = "You have no active service request.\nCreate one.",
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF4E616D))
+  }
+}
+
+@Composable
+fun CategoriesFiltersSection() {
+  Row(
+      modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+      horizontalArrangement = Arrangement.SpaceBetween,
+      verticalAlignment = Alignment.CenterVertically) {
+        Box(modifier = Modifier.background(Color.Blue, shape = RoundedCornerShape(16.dp))) {
+          Row(
+              modifier = Modifier.padding(horizontal = 32.dp, vertical = 16.dp),
+              verticalAlignment = Alignment.CenterVertically,
+              horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.filter_square),
+                    contentDescription = "categories filter",
+                    modifier = Modifier.size(24.dp))
+                Text(text = "Category Settings", fontWeight = FontWeight.Bold, color = Color.White)
+              }
+        }
+        Box(
+            modifier = Modifier.background(Color.Blue, shape = RoundedCornerShape(16.dp)),
+        ) {
+          Row(
+              modifier = Modifier.padding(horizontal = 32.dp, vertical = 16.dp),
+              verticalAlignment = Alignment.CenterVertically,
+              horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.filter_circle),
+                    contentDescription = "categories sort",
+                    modifier = Modifier.size(24.dp))
+                Text(text = "Sort", fontWeight = FontWeight.Bold, color = Color.White)
+              }
+        }
+      }
 }
 
 @Composable
 fun RequestItemRow(request: ServiceRequest, onClick: () -> Unit) {
   // Use a Box to wrap the entire row and the bottom bar
+  val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+  val date = dateFormat.format(request.dueDate.toDate())
   Box(
       modifier =
           Modifier.fillMaxWidth()
-              .background(color = Color.Transparent)
-              .padding(start = 16.dp, end = 16.dp)
+              .clickable { onClick() }
+              .border(1.dp, Color.LightGray, RoundedCornerShape(16.dp))
+              .padding(horizontal = 16.dp, vertical = 12.dp)
               .testTag("requestListItem")) {
-        Button(
-            onClick = onClick,
-            modifier = Modifier.fillMaxWidth().height(80.dp).background(color = Color.Transparent),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-        ) {
+        Column {
+          Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            AsyncImage(
+                model = request.imageUrl,
+                placeholder = painterResource(id = R.drawable.loading),
+                error = painterResource(id = R.drawable.error),
+                contentDescription = "service request image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(48.dp).clip(RoundedCornerShape(12.dp)))
+            Column {
+              Text(text = request.title, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+              Text(
+                  text = request.type.name.lowercase().replaceFirstChar { it.uppercase() },
+                  fontSize = 14.sp,
+              )
+            }
+          }
+          Spacer(modifier = Modifier.size(12.dp))
           Row(
-              horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
-              verticalAlignment = Alignment.Top,
-              modifier = Modifier.fillMaxWidth()) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier.weight(1f)) {
-                      val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-                      val date = dateFormat.format(request.dueDate.toDate())
-                      Text(
-                          text = "Deadline: $date",
-                          style =
-                              TextStyle(
-                                  fontSize = 12.sp,
-                                  lineHeight = 16.sp,
-                                  fontFamily = FontFamily.Default,
-                                  fontWeight = FontWeight(500),
-                                  color = Color(0xFF49454F),
-                                  letterSpacing = 0.5.sp,
-                              ))
-                      Text(
-                          text = request.title,
-                          style =
-                              TextStyle(
-                                  fontSize = 16.sp,
-                                  lineHeight = 24.sp,
-                                  fontFamily = FontFamily.Default,
-                                  fontWeight = FontWeight(400),
-                                  color = Color(0xFF191C1E),
-                                  letterSpacing = 0.5.sp,
-                              ))
-                      Text(
-                          text = request.type.name.lowercase().replaceFirstChar { it.uppercase() },
-                          style =
-                              TextStyle(
-                                  fontSize = 14.sp,
-                                  lineHeight = 20.sp,
-                                  fontFamily = FontFamily.Default,
-                                  fontWeight = FontWeight(400),
-                                  color = Color(0xFF49454F),
-                                  letterSpacing = 0.25.sp,
-                              ))
-                    }
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
-                    verticalAlignment = Alignment.Top,
-                ) {
-                  Text(
-                      text =
-                          request.status.name.replace("_", " ").lowercase().replaceFirstChar {
-                            it.uppercase()
-                          },
-                      style =
-                          TextStyle(
-                              fontSize = 11.sp,
-                              lineHeight = 16.sp,
-                              fontFamily = FontFamily.Default,
-                              fontWeight = FontWeight(500),
-                              color = getStatusColor(request.status),
-                              textAlign = TextAlign.Right,
-                              letterSpacing = 0.5.sp,
-                          ))
-                  Icon(
-                      imageVector = Icons.Default.PlayArrow,
-                      contentDescription = "Navigate",
-                      tint = Color.Gray)
+              modifier = Modifier.fillMaxWidth(),
+              horizontalArrangement = Arrangement.SpaceBetween) {
+                Text(
+                    text = request.status.name.lowercase().replaceFirstChar { it.uppercase() },
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = getStatusColor(request.status))
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                  Text(text = "Until:", fontSize = 14.sp)
+                  Text(text = date, fontWeight = FontWeight.Bold, fontSize = 14.sp)
                 }
               }
         }
-
-        Box(
-            modifier =
-                Modifier.fillMaxWidth()
-                    .height(1.dp)
-                    .align(Alignment.BottomCenter)
-                    .background(Color.LightGray))
       }
 }
 
