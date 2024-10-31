9b769097d392f411752ed968c4c34593b4e4ff2a
jamaly <el.jamaly@epfl.ch>
2024-10-31 08:57:36 +0100

    feat(backend): implement logic to retrieve cached locations

--- app/src/main/java/com/android/solvit/seeker/model/profile/SeekerProfile.kt
+++ app/src/main/java/com/android/solvit/seeker/model/profile/SeekerProfile.kt
@@ -1,10 +1,13 @@
 package com.android.solvit.seeker.model.profile
 
+import com.android.solvit.shared.model.map.Location
+
 data class SeekerProfile(
     val uid: String,
     val name: String = "",
     val username: String = "",
     val email: String = "",
     val phone: String = "",
-    val address: String = ""
+    val address: String = "",
+    val cachedLocations: List<Location> = emptyList()
 )
