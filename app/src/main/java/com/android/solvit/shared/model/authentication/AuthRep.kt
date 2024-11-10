0a70864276ec04ba47628e6d2a6d073b5ae4458b
jamaly <el.jamaly@epfl.ch>
2024-11-10 15:29:30 +0100

    refactor(auth): separate AuthRepository into interface for improved mocking and testing

---
+++ app/src/main/java/com/android/solvit/shared/model/authentication/AuthRep.kt
@@ -0,0 +1,37 @@
+package com.android.solvit.shared.model.authentication
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+
+interface AuthRep {
+
+    fun init(onSuccess: (user: User?) -> Unit)
+    fun getUserId(): String
+    fun loginWithEmailAndPassword(
+        email: String,
+        password: String,
+        onSuccess: (user: User) -> Unit,
+        onFailure: (Exception) -> Unit
+    )
+    fun signInWithGoogle(
+        account: GoogleSignInAccount,
+        onSuccess: (user: User) -> Unit,
+        onFailure: (Exception) -> Unit
+    )
+
+    fun registerWithEmailAndPassword(
+        role: String,
+        email: String,
+        password: String,
+        onSuccess: (user: User) -> Unit,
+        onFailure: (Exception) -> Unit
+    )
+
+    fun registerWithGoogle(
+        account: GoogleSignInAccount,
+        role: String,
+        onSuccess: (user: User) -> Unit,
+        onFailure: (Exception) -> Unit
+    )
+
+    fun logout(onSuccess: () -> Unit)
+}
