44a9456cb72e974c773c0cc20540c4501ad727ff
Achille Triomphe <achille.triomphe@epfl.ch>
2024-11-13 09:17:42 +0100

    Fix merging errors

--- app/src/main/java/com/android/solvit/shared/ui/authentication/OpeningScreen.kt
+++ app/src/main/java/com/android/solvit/shared/ui/authentication/OpeningScreen.kt
@@ -2,6 +2,7 @@ package com.android.solvit.shared.ui.authentication
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -52,7 +53,6 @@ fun OpeningScreenPortrait(navigationActions: NavigationActions) {
       color = colorScheme.background) {
         Column(
             modifier = Modifier.fillMaxSize().background(colorScheme.background).padding(16.dp),
-            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
               Image(
