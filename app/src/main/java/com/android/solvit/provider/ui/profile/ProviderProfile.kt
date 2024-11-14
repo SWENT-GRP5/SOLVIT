024be094b8ed9a60b031673f20982a03b28deb28
Achille Triomphe <achille.triomphe@epfl.ch>
2024-11-14 18:21:18 +0100

    style: Format code with ktfmt

--- app/src/main/java/com/android/solvit/provider/ui/profile/ProviderProfile.kt
+++ app/src/main/java/com/android/solvit/provider/ui/profile/ProviderProfile.kt
@@ -49,7 +49,6 @@ import com.android.solvit.seeker.model.provider.ListProviderViewModel
 import com.android.solvit.shared.model.authentication.AuthViewModel
 import com.android.solvit.shared.model.provider.Provider
 import com.android.solvit.shared.ui.navigation.NavigationActions
-import com.android.solvit.shared.ui.navigation.Route
 
 @Composable
 fun ProviderProfileScreen(
