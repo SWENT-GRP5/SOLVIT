239007626e548796047516597f377c39b9074746
Achille Triomphe <achille.triomphe@epfl.ch>
2024-11-14 14:28:07 +0100

    Fixed navigation between screens to also save the previous screen

--- app/src/main/java/com/android/solvit/shared/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/android/solvit/shared/ui/navigation/NavigationActions.kt
@@ -125,11 +125,7 @@ open class NavigationActions(
    * @param screen The screen to navigate to
    */
   open fun navigateTo(screen: String) {
-    navController.navigate(screen) {
-        popUpTo(navController.graph.startDestinationId) { saveState = true }
-            launchSingleTop = true
-            restoreState = true
-    }
+    navController.navigate(screen)
   }
 
   /** Navigate back to the previous screen. */
