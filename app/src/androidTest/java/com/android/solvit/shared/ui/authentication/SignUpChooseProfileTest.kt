17c6c5976ae1b98662f88b3ec3c71ae2de16a68e
Achille Triomphe <achille.triomphe@epfl.ch>
2024-11-14 03:03:21 +0100

    Removed unused directive

--- app/src/androidTest/java/com/android/solvit/shared/ui/authentication/SignUpChooseProfileTest.kt
+++ app/src/androidTest/java/com/android/solvit/shared/ui/authentication/SignUpChooseProfileTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.solvit.shared.ui.navigation.NavigationActions
-import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
