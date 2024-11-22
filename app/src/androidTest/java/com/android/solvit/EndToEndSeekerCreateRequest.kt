cfcb5411008a262772092068ee0ece7c98097422
Manu Cristini <manu.cristini@epfl.ch>
2024-11-22 14:12:42 +0100

    fix: update end2end to match new behavior

--- app/src/androidTest/java/com/android/solvit/EndToEndSeekerCreateRequest.kt
+++ app/src/androidTest/java/com/android/solvit/EndToEndSeekerCreateRequest.kt
@@ -239,7 +239,8 @@ class EndToEndSeekerCreateRequest {
     }
     composeTestRule.onNodeWithTag("deleteRequestButton").performClick()
     composeTestRule.waitUntil(timeoutMillis = 10000) {
-      composeTestRule.onNodeWithTag("edit_button").isDisplayed()
+      composeTestRule.onNodeWithTag("requestsOverviewScreen").isDisplayed()
     }
+    composeTestRule.onNodeWithTag("noServiceRequestsScreen").assertIsDisplayed()
   }
 }
