e04bab6c4801068e3d9e7b6c8ddf3c2d0eb833e7
jamaly <el.jamaly@epfl.ch>
2024-12-03 22:08:07 +0100

    fix : add some missing parameters in test

--- app/src/androidTest/java/com/android/solvit/shared/ui/chat/AiSolverChat.kt
+++ app/src/androidTest/java/com/android/solvit/shared/ui/chat/AiSolverChat.kt
@@ -4,18 +4,27 @@ import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.navigation.NavController
+import com.android.solvit.shared.ui.navigation.NavigationActions
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.mock
 
 class AiSolverChat {
   @get:Rule val composeTestRule = createComposeRule()
+  private lateinit var navController: NavController
+  private lateinit var navigationActions: NavigationActions
 
-  @Before fun SetUp() {}
+  @Before fun SetUp() {
+    navController = mock(NavController::class.java)
+    navigationActions = NavigationActions(navController)
+
+  }
 
   @Test
   fun AllComponentsAreDisplayedAiWelcomeScreen() {
-    composeTestRule.setContent { AiSolverWelcomeScreen() }
+    composeTestRule.setContent { AiSolverWelcomeScreen(navigationActions) }
     composeTestRule.onNodeWithTag("AiGetStartedScreen").assertIsDisplayed()
     composeTestRule.onNodeWithTag("getStartedButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("getStartedButton").assertHasClickAction()
@@ -25,7 +34,7 @@ class AiSolverChat {
 
   @Test
   fun AllComponentsAreDisplayedAiChatScreen() {
-    composeTestRule.setContent { AiSolverScreen() }
+    composeTestRule.setContent { AiSolverScreen(navigationActions) }
     composeTestRule.onNodeWithTag("AiSolverScreen").assertIsDisplayed()
     composeTestRule.onNodeWithTag("AiChatHeader").assertIsDisplayed()
     composeTestRule.onNodeWithTag("SendMessageBar").assertIsDisplayed()
