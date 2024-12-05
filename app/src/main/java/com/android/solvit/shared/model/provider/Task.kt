4998b44ff91bd524399bf45743cdcbfc8f18fb15
ethanboren <ethan.boren@epfl.ch>
2024-12-05 13:13:34 +0100

    chore: Removed 2 unnecessary files

--- app/src/main/java/com/android/solvit/shared/model/provider/Task.kt
+++
@@ -1,9 +0,0 @@
-package com.android.solvit.shared.model.provider
-
-data class Task(
-    val id: String,
-    val providerId: String, // Reference to the provider
-    val description: String,
-    val price: Double,
-    val status: String // e.g., "completed", "pending"
-)
