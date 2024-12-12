da7e135b72fcb08f9b1811e78fd9bcd710b414bb
jamaly <el.jamaly@epfl.ch>
2024-12-12 20:11:51 +0100

    fix : uncomment failling test

--- app/src/androidTest/java/com/android/solvit/seeker/ui/request/GeminiImageAnalyzerTest.kt
+++ app/src/androidTest/java/com/android/solvit/seeker/ui/request/GeminiImageAnalyzerTest.kt
@@ -1,11 +1,14 @@
 package com.android.solvit.seeker.ui.request
 
+import android.graphics.Bitmap
+import com.android.solvit.shared.model.request.analyzer.GeminiImageAnalyzer
 import io.mockk.*
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Test
 
 class GeminiImageAnalyzerTest {
 
-  /*
   @Test
   fun analyzeImages_integrationTest() = runTest {
     val bitmaps = listOf(Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888))
@@ -18,5 +21,5 @@ class GeminiImageAnalyzerTest {
     assertNotNull(result.first)
     assertNotNull(result.second)
     assertNotNull(result.third)
-  }*/
+  }
 }
