290522a6dc05f1587c1c08702746e58065be938e
jamaly <el.jamaly@epfl.ch>
2024-12-12 22:33:26 +0100

    fix : improve robustness of tests sign up create review

--- app/src/androidTest/java/com/android/solvit/shared/ui/authentication/SignUpTest.kt
+++ app/src/androidTest/java/com/android/solvit/shared/ui/authentication/SignUpTest.kt
@@ -39,6 +39,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_testAllTheTest() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("goBackButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("signUpIllustration").assertIsDisplayed()
@@ -55,6 +56,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_testAllPerformClick() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("goBackButton").performClick()
     composeTestRule.onNodeWithTag("passwordInputField").performClick()
@@ -77,6 +79,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_errorShowInEmailField() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("emailErrorMessage").isNotDisplayed()
     composeTestRule.onNodeWithTag("emailInputField").performTextInput("test@example")
@@ -89,6 +92,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_errorShowInPasswordField() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("passwordErrorMessage").isNotDisplayed()
     composeTestRule.onNodeWithTag("passwordInputField").performTextInput("12345")
@@ -101,6 +105,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_errorShowInConfirmPasswordField() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
 
     composeTestRule.onNodeWithTag("confirmPasswordErrorMessage").isNotDisplayed()
     composeTestRule.onNodeWithTag("confirmPasswordInputField").performTextInput("12345")
@@ -113,6 +118,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_emailInput() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
 
     // Test email input
     composeTestRule.onNodeWithTag("emailInputField").performTextInput("test@example.com")
@@ -121,6 +127,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_googleSignInReturnsValidActivityResult() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
     composeTestRule.onNodeWithTag("googleSignUpButton").performClick()
     // assert that an Intent resolving to Google Mobile Services has been sent (for sign-in)
     Intents.intended(IntentMatchers.toPackage("com.google.android.gms"))
@@ -141,6 +148,7 @@ class SignUpScreenTest {
   @Test
   fun signUp_signUpButtonNavigatesToChooseRoleScreen() {
     composeTestRule.setContent { SignUpScreen(mockNavigationActions) }
+    composeTestRule.waitForIdle()
     composeTestRule.onNodeWithTag("emailInputField").performTextInput("test@test.com")
     composeTestRule.onNodeWithTag("passwordInputField").performTextInput("password")
     composeTestRule.onNodeWithTag("confirmPasswordInputField").performTextInput("password")
