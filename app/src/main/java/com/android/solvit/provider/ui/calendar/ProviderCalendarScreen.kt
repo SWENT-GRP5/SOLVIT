a4e1d542455c374188297ed1555688af8478790d
Achille Triomphe <achille.triomphe@epfl.ch>
2024-12-17 02:50:28 +0100

    KTFmt Format

--- app/src/main/java/com/android/solvit/provider/ui/calendar/ProviderCalendarScreen.kt
+++ app/src/main/java/com/android/solvit/provider/ui/calendar/ProviderCalendarScreen.kt
@@ -10,15 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
